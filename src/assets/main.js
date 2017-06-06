25e8c2b0c5cb1cc1eee31abd7985e3d3b88d439e
Alexandre ROBILLARD <alexandre.robillard@gmail.com>
2017-06-06 22:38:50 +0200

    ARO Debut et fin projet CS

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,26 @@
 $(function() {
 
   // your code will go here
+  $.ajax({
+    url: 'https://www.codeschool.com/users/2531550.json',
+    dataType: 'jsonp',
+    success: function(response){
+      console.log('response', response.courses.completed);
+      ajoutBadges(response.courses.completed);
+    }
+  });
+
+  function ajoutBadges(formations){
+
+    var $badges = $('#badges')
+
+    formations.forEach(function(formations){
+      $('<div class="course">'+
+      '<h3>'+formations.title+'</h3>'+
+      '<img src="'+formations.badge+'"></img>'+
+      '<a href="'+formations.url+'" class="btn btn-primary" target="_blank">See Course</a>'+
+      '</div>').appendTo($badges);
+    })
+  }
 
 });
